28465f3fc5879b31f0f1d194311eae0aa67c4b48
s0nzai <ut.intelligam3@gmail.com>
2021-05-09 15:22:27 +0900

    remove unneeded parenthesis in pretty-print

--- form.ml
+++ form.ml
@@ -9,19 +9,20 @@ type t =
 
 let compare = Stdlib.compare
 
+let rm_paren s =
+    if s.[0] = '('
+    then String.sub s 1 (String.length s - 2)
+    else s
+
 let rec string_of_ = function
     | Empty -> ""
     | Var(v) -> v
     | Not(p) -> "\\lnot " ^ (string_of_ p)
-    | Impl(p, q) -> "(" ^ (string_of_ p) ^ "\\to " ^ (string_of_ q) ^ ")"
+    | Impl(p, q) -> "(" ^ (string_of_ p) ^ "\\to " ^ (rm_paren (string_of_ q)) ^ ")"
     | And(p, q) -> "(" ^ (string_of_ p) ^ "\\land " ^ (string_of_ q) ^ ")"
     | Or(p, q) -> "(" ^ (string_of_ p) ^ "\\lor " ^ (string_of_ q) ^ ")"
 
-let string_of p =
-    let sp = string_of_ p in
-    match p with
-    | Var(_) | Not(_) -> sp
-    | _ -> String.sub sp 1 (String.length sp - 2)
+let string_of p = rm_paren (string_of_ p)
 
 let is_atomic = function
     | Var(_) -> true
